a7bcd28f52015c366f1b9c13e2753b8e43b3338b
brijesh <ibrijeshshingala@gmail.com>
2024-10-07 14:08:27 +0530

    07/0/2024

---
+++ khatabook/src/main/java/com/khatabook/khatabook/KhataBookApplication.java
@@ -0,0 +1,13 @@
+package com.khatabook.khatabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhataBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhataBookApplication.class, args);
+	}
+
+}
