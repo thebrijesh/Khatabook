d609219fec55e3886eb01e7fa823686a6464b562
mryash08 <yashchodvadiya203@gmail.com>
2024-10-09 10:26:15 +0530

    Add Transaction Related Api

--- khatabook/src/main/java/com/khatabook/khatabook/Model/PartyTransaction.java
+++ khatabook/src/main/java/com/khatabook/khatabook/Model/PartyTransaction.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class PartyTransaction  extends BaseModel{
-    @OneToOne(cascade = CascadeType.MERGE)
+    @ManyToOne(cascade = CascadeType.MERGE)
     Party party;
     double amount;
     Date date;
--- khatabook/src/main/java/com/khatabook/khatabook/services/implementetion/TransactionServiceImpl.java
+++ khatabook/src/main/java/com/khatabook/khatabook/services/implementetion/TransactionServiceImpl.java
@@ -1,8 +1,24 @@
 package com.khatabook.khatabook.services.implementetion;
 
+import com.khatabook.khatabook.Model.PartyTransaction;
+import com.khatabook.khatabook.repository.TransactionRepository;
 import com.khatabook.khatabook.services.interfaces.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
+    @Autowired
+    TransactionRepository transactionRepository;
+    @Override
+    public List<PartyTransaction> getAllTransaction(Long partyId) {
+        return transactionRepository.findAllByPartyId(partyId);
+    }
+
+    @Override
+    public PartyTransaction createBill(PartyTransaction partyTransaction) {
+        return transactionRepository.save(partyTransaction);
+    }
 }
